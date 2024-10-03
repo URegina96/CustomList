1accff67d98e3be918b51d1ff549dd23d7811691
regina <usmanova.re@yandex.ru>
2024-10-03 19:48:40 +0500

    подготовка структуры проекта

---
+++ app/src/main/java/com/example/customlist/FragmentUp.java
@@ -0,0 +1,13 @@
+package com.example.customlist;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentUp extends Fragment {
+
+}
---
+++ app/src/main/java/com/example/customlist/ItemList.java
@@ -0,0 +1,4 @@
+package com.example.customlist;
+
+public class ItemList {
+}
