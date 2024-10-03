8eebe65f1bb927a2fbbf671bc465fd6e4c12aa42
regina <usmanova.re@yandex.ru>
2024-10-03 19:52:38 +0500

    Код для предоставления информации

--- app/src/main/java/com/example/customlist/DetailListInfo.java
+++ app/src/main/java/com/example/customlist/DetailListInfo.java
@@ -1,4 +1,38 @@
 package com.example.customlist;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class DetailListInfo {
+    // Метод для получения списка объектов в виде ArrayList, состоящего из HashMap
+    public static ArrayList<HashMap<String, String>> getItemList() {
+        ArrayList<HashMap<String, String>> list = new ArrayList<>(); // Создает новый список для хранения объектов
+
+        // Цикл для создания 30 объектов
+        for(int i = 0; i < 30; i++) {
+            HashMap<String, String> map = new HashMap<>(); // Создает новый HashMap для каждого объекта
+            map.put("ID", String.valueOf(i)); // Добавляет ID объекта в HashMap
+            map.put("NAME", "Объект номер " + i); // Добавляет имя объекта в HashMap
+            list.add(map); // Добавляет HashMap в список
+        }
+        return list; // Возвращает завершенный список объектов
+    }
+
+    // Метод для получения объекта по его ID
+    public static ItemList getItemById(int id) {
+        ArrayList<ItemList> l = new ArrayList<>(); // Создает новый список для хранения объектов типа ObjectItem
+
+        // Цикл для создания 30 объектов типа ObjectItem
+        for(int i = 0; i < 30; i++) {
+            ItemList item = new ItemList(); // Создает новый объект ObjectItem
+            item.setId(i); // Устанавливает ID объекта
+            item.setName("Объект " + i); // Устанавливает имя объекта
+            item.setShortDescription("Короткое описание объекта " + i); // Устанавливает короткое описание объекта
+            item.setLongDescription("Какое-то очень длинное описание объекта " + i + "\n" + // Устанавливает длинное описание объекта
+                    "Мы все знаем, что этот объект имеет очень длинное описание");
+            item.setImgName("image_" + i); // Устанавливает название изображения объекта
+            l.add(item); // Добавляет созданный объект в список
+        }
+        return l.get(id); // Возвращает объект по его ID из списка
+    }
 }
--- app/src/main/java/com/example/customlist/ItemList.java
+++ app/src/main/java/com/example/customlist/ItemList.java
@@ -1,4 +1,41 @@
 package com.example.customlist;
 
 public class ItemList {
+    private String name; // Поле для хранения имени объекта
+    private int id; // Поле для хранения идентификатора объекта
+    private String shortDescription; // Поле для хранения краткого описания объекта
+    private String longDescription; // Поле для хранения полного описания объекта
+    private String imgName; // Поле для хранения имени изображения объекта
+
+    public String getName() { // Метод для получения имени объекта
+        return name; // Возвращает значение поля name
+    }
+
+    public void setName(String name) { // Метод для установки имени объекта
+        this.name = name; // Устанавливает значение поля name
+    }
+
+    public int getId() { // Метод для получения идентификатора объекта
+        return id; // Возвращает значение поля id
+    }
+
+    public void setId(int id) { // Метод для установки идентификатора объекта
+        this.id = id; // Устанавливает значение поля id
+    }
+
+    public String getShortDescription() { // Метод для получения краткого описания объекта
+        return shortDescription; // Возвращает значение поля shortDescription
+    }
+
+    public void setShortDescription(String shortDescription) { // Метод для установки краткого описания объекта
+        this.shortDescription = shortDescription; // Устанавливает значение поля shortDescription
+    }
+
+    public void setLongDescription(String longDescription) { // Метод для установки полного описания объекта
+        this.longDescription = longDescription; // Устанавливает значение поля longDescription
+    }
+
+    public void setImgName(String imgName) { // Метод для установки имени изображения объекта
+        this.imgName = imgName; // Устанавливает значение поля imgName
+    }
 }
